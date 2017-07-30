14d44f8072d1eda8d5e61ba65d9c66cfef171e5e
Ivan Bor <borisevich.ivan1994@gmail.com>
2017-07-30 16:51:07 +0300

    add empty activity launched on widget click

--- app/src/main/java/com/example/ivanbor/widgetexample/MainWidget.java
+++ app/src/main/java/com/example/ivanbor/widgetexample/MainWidget.java
@@ -1,9 +1,12 @@
 package com.example.ivanbor.widgetexample;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import java.util.Arrays;
 
@@ -14,24 +17,38 @@ public class MainWidget extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
-        Log.d(LOG_TAG, "onEnabled: first widget example is created");
+        Log.i(LOG_TAG, "onEnabled: first widget example is created");
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-        Log.d(LOG_TAG, "onUpdate: widgets " + Arrays.toString(appWidgetIds));
+        launchActivity(context, appWidgetManager, appWidgetIds);
+
+        Log.i(LOG_TAG, "onUpdate: widgets " + Arrays.toString(appWidgetIds));
     }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         super.onDeleted(context, appWidgetIds);
-        Log.d(LOG_TAG, "onDeleted: widgets " + Arrays.toString(appWidgetIds) + " were deleted");
+        Log.i(LOG_TAG, "onDeleted: widgets " + Arrays.toString(appWidgetIds) + " were deleted");
     }
 
     @Override
     public void onDisabled(Context context) {
         super.onDisabled(context);
-        Log.d(LOG_TAG, "onDisabled: last widget example were deleted");
+        Log.i(LOG_TAG, "onDisabled: last widget example were deleted");
+    }
+
+    private void launchActivity(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.main_widget);
+        Intent configIntent = new Intent(context, MainActivity.class);
+
+        PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+
+        remoteViews.setOnClickPendingIntent(R.id.textView, configPendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+
+        Log.i(LOG_TAG, "Main activity is launched!");
     }
 }
